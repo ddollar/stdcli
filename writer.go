55374a037e8a7788de428ae85a0fe3dee07a5390
David Dollar <david@dollar.io>
2018-05-27 17:07:55 -0400

    change id color

--- writer.go
+++ writer.go
@@ -30,7 +30,7 @@ func init() {
 			"header": RenderColors(242),
 			"h1":     RenderColors(244),
 			"h2":     RenderColors(241),
-			"id":     RenderColors(24),
+			"id":     RenderColors(247),
 			"info":   RenderColors(247),
 			"ok":     RenderColors(46),
 			"start":  RenderColors(247),
